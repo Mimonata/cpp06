c2507521b8b4d9647274ec7725bb6491fd82693d
Mimonata <sabina_pitul@yahoo.de>
2025-07-18 06:47:44 +0200

    ex02

---
+++ ex02/Base.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 21:08:39 by spitul            #+#    #+#             */
+/*   Updated: 2025/07/18 06:47:05 by spitul           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <typeinfo>
+
+Base::~Base(){}
+
+Base	*generate(void)
+{
+	std::srand(std::time(0));
+	int	r = std::rand() % 3;
+	
+	switch(r)
+	{
+		case 0:
+			return new A();
+		case 1:
+			return new B();
+		case 2:
+			return new C();
+		default:
+			return NULL;
+	}
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "Object class type pointed to = A\n";
+	else if (dynamic_cast<B*>(p))
+		std::cout << "Object class type pointed to = B\n";	
+	else if (dynamic_cast<C*>(p))
+		std::cout << "Object class type pointed to = C\n";
+}
+
+void identify(Base& p)
+{
+	try
+	{
+		A &tmp = dynamic_cast<A&>(p);
+		std::cout << "Object class type = A\n";
+		return ;
+	}
+	catch(const std::bad_cast& e)
+	{
+	}
+
+	try
+	{
+		B &tmp = dynamic_cast<B&>(p);
+		std::cout << "Object class type = B\n";
+		return ;
+
+	}
+	catch(const std::bad_cast& e)
+	{
+	}
+	
+	try
+	{
+		C &tmp = dynamic_cast<C&>(p);
+		std::cout << "Object class type = C\n";
+		return ;
+	}
+	catch(const std::bad_cast& e)
+	{
+	}
+	
+	std::cout << "Unknown type\n";
+}
--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -1,43 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: spitul <spitul@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/15 16:34:32 by spitul            #+#    #+#             */
-/*   Updated: 2025/07/17 21:00:10 by spitul           ###   ########.fr       */
+/*   Created: 2025/07/17 21:00:38 by spitul            #+#    #+#             */
+/*   Updated: 2025/07/17 21:08:22 by spitul           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Serializer.hpp"
+#ifndef BASE_HPP
+#define BASE_HPP
 
-Serializer::Serializer()
+class Base
 {
-}
+	public:
+		virtual ~Base();
+};
 
-Serializer::~Serializer()
+class A	: public Base
 {
-}
-
-Serializer::Serializer(const Serializer &other)
-{
-	(void)other;
-}
-		
-Serializer& Serializer::operator=(const Serializer &other)
-{
-	(void)other;
 	
-	return *this;
-}
+};
 
-uintptr_t	Serializer::serialize(Data* ptr)
+class B	: public Base
 {
-	return reinterpret_cast<uintptr_t>(ptr);
-}
+	
+};
 
-Data*	Serializer::deserialize(uintptr_t raw)
+class C	: public Base
 {
-	return reinterpret_cast<Data*>(raw);
-}
+	
+};
+
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
+
+#endif
